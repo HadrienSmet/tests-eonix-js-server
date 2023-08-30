3e26a1979b141b58b711ab2edebb40982ba57113
Hadrien <smethadrien.96@gmail.com>
2023-08-30 11:51:30 +0200

    Fixed the environment variable linking to the db

--- dbConfig.js
+++ dbConfig.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose
-    .connect(
-        "mongodb+srv://dbAdmin:CF7ZethZEATzfpgA@cluster0.hktpuz8.mongodb.net/?retryWrites=true&w=majority",
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }
-    )
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => console.log("Connexion à MongoDB réussie !"))
     .catch(() => console.log("Connexion à MongoDB échouée !"));
